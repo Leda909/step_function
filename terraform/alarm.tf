23623f4f3bcfceef925cfad9c83a90435798ccb4
Leda909 <ko9zad@gmail.com>
2025-06-02 06:40:59 +0100

    in progress step function and cloudewatchs

---
+++ terraform/alarm.tf
@@ -0,0 +1,50 @@
+resource "aws_cloudwatch_metric_alarm" "lambda_one_error_alarm" {
+  alarm_name          = "lambda-one-error-alarm"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 1
+  metric_name         = "Errors"
+  namespace           = "AWS/Lambda"
+  period              = 300
+  statistic           = "Sum"
+  threshold           = 0
+  alarm_description   = "Alarm when Lambda One has any errors"
+  alarm_actions       = [aws_sns_topic.alerts.arn]
+
+  dimensions = {
+    FunctionName = aws_lambda_function.lambda_one.function_name
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "lambda_two_error_alarm" {
+  alarm_name          = "lambda-two-error-alarm"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 1
+  metric_name         = "Errors"
+  namespace           = "AWS/Lambda"
+  period              = 300
+  statistic           = "Sum"
+  threshold           = 0
+  alarm_description   = "Alarm when Lambda Two has any errors"
+  alarm_actions       = [aws_sns_topic.alerts.arn]
+
+  dimensions = {
+    FunctionName = aws_lambda_function.lambda_two.function_name
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "lambda_three_error_alarm" {
+  alarm_name          = "lambda-three-error-alarm"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 1
+  metric_name         = "Errors"
+  namespace           = "AWS/Lambda"
+  period              = 300
+  statistic           = "Sum"
+  threshold           = 0
+  alarm_description   = "Alarm when Lambda Three has any errors"
+  alarm_actions       = [aws_sns_topic.alerts.arn]
+
+  dimensions = {
+    FunctionName = aws_lambda_function.lambda_three.function_name
+  }
+}
