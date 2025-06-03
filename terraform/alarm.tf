bbece103dd95a8641362079030e69318f52ed79b
Leda909 <ko9zad@gmail.com>
2025-06-03 18:03:09 +0100

    update s3 bucket name

--- terraform/alarm.tf
+++ terraform/alarm.tf
@@ -49,3 +49,20 @@ resource "aws_cloudwatch_metric_alarm" "lambda_three_error_alarm" {
     FunctionName = aws_lambda_function.lambda_three.function_name
   }
 }
+
+resource "aws_cloudwatch_metric_alarm" "step_function_error_alarm" {
+  alarm_name          = "step_function-error-alarm"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 1
+  metric_name         = "Errors"
+  namespace           = "AWS/Lambda"
+  period              = 300
+  statistic           = "Sum"
+  threshold           = 0
+  alarm_description   = "Alarm when Lambda One has any errors"
+  alarm_actions       = [aws_sns_topic.alerts.arn]
+
+  dimensions = {
+    FunctionName = aws_lambda_function.lambda_one.function_name
+  }
+}
--- terraform/event.tf
+++ terraform/event.tf
@@ -14,3 +14,20 @@ resource "aws_cloudwatch_event_target" "trigger_step_function" {
   arn       = aws_sfn_state_machine.etl_workflow.arn
   role_arn  = aws_iam_role.eventbridge_to_step_function.arn
 }
+
+# Schedule Step Function
+# resource "aws_scheduler_schedule" "step" {  ##TODO change the name
+#   name       = "my-step-function-scheduler"
+#   #group_name = "step-function-group"
+
+#   flexible_time_window {
+#     mode = "OFF"
+#   }
+
+#   schedule_expression = "rate(2 minute)"
+
+#   target {
+#     arn      = aws_sfn_state_machine.sfn_state_machine.arn
+#     role_arn = aws_iam_role.step_function_role.arn
+#   }
+# }
